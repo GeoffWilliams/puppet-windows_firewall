a64029b25c14f01f3d529ffd82fe790ffd6554ba
Alexandre JARDON <28548335+webalexeu@users.noreply.github.com>
2020-12-17 10:44:27 +0100

    move from require to require_relative

--- lib/puppet/provider/windows_firewall_profile/ruby.rb
+++ lib/puppet/provider/windows_firewall_profile/ruby.rb
@@ -1,5 +1,5 @@
 require 'puppet_x'
-require 'puppet_x/windows_firewall'
+require_relative '../../../puppet_x/windows_firewall'
 
 Puppet::Type.type(:windows_firewall_profile).provide(:windows_firewall_profile, :parent => Puppet::Provider) do
   confine :osfamily => :windows
--- lib/puppet/provider/windows_firewall_rule/ruby.rb
+++ lib/puppet/provider/windows_firewall_rule/ruby.rb
@@ -1,5 +1,5 @@
 require 'puppet_x'
-require 'puppet_x/windows_firewall'
+require_relative '../../../puppet_x/windows_firewall'
 
 Puppet::Type.type(:windows_firewall_rule).provide(:windows_firewall_rule, :parent => Puppet::Provider) do
   confine :osfamily => :windows
